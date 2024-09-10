fc79f24c243435a2482ef0b4bbbe995d393bdeb2
toto <tototianhao@gmail.com>
2024-09-10 16:24:14 +0800

    Auto update

---
+++ lark/alarm/lark_api.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+
+def send_alert_to_feishu(webhook_url, alert_message):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    # 创建要发送的消息内容
+    data = {
+        "msg_type": "text",  # 消息类型，这里是文本消息
+        "content": {
+            "text": alert_message  # 警报消息内容
+        }
+    }
+    # 发送 POST 请求到飞书 Webhook
+    response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
+
+    # 打印返回结果
+    if response.status_code == 200:
+        print("警报发送成功")
+    else:
+        print(f"警报发送失败: {response.status_code}, {response.text}")
--- utils/fastapi_exception.py
+++ utils/fastapi_exception.py
@@ -1,10 +1,14 @@
 import http
 import json
+import traceback
+
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
+from python_ai_utils.lark.alarm.lark_api import send_alert_to_feishu
 
-def error_in_file(app, log_tool):
+
+def error_in_file(app, log_tool, lark_webhook_url=None):
     @app.middleware("http")
     async def save_request_body(request: Request, call_next):
         body = await request.body()
@@ -28,11 +32,12 @@ def error_in_file(app, log_tool):
             body = getattr(request.state, 'body', None)
         except Exception:
             body = None
-        log_tool.error(f"请求路径: {path}")
-        log_tool.error(f"请求头: {headers}")
-        log_tool.error(f"请求查询参数: {query_params}")
-        log_tool.error(f"请求体: {body}")
+        error_message = f"请求路径: {path}\n请求头: {headers}\n请求查询参数: {query_params}\n请求体: {body}"
+        log_tool.error(error_message)
         log_tool.error(f"{e}", exc_info=True)
+        if lark_webhook_url:
+            error_message_with_stack = error_message + f"错误信息: {str(e)}\n 堆栈信息: {traceback.format_exc()}"
+            send_alert_to_feishu(lark_webhook_url, error_message_with_stack)
         return JSONResponse(
             status_code=http.HTTPStatus.INTERNAL_SERVER_ERROR,
             content={"message": str(e)},
