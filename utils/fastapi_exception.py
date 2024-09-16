55c66053d547907fc9fb97baf363c01acf50c788
toto <tototianhao@gmail.com>
2024-09-17 01:37:58 +0800

    Auto update

--- utils/fastapi_exception.py
+++ utils/fastapi_exception.py
@@ -2,6 +2,7 @@ import http
 import json
 import traceback
 
+from fastapi import HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
@@ -56,4 +57,33 @@ def error_in_file(app, log_tool, lark_webhook_url=None, server_name=''):
         return JSONResponse(
             status_code=http.HTTPStatus.INTERNAL_SERVER_ERROR,
             content={"message": str(e)},
+        )
+
+    @app.exception_handler(HTTPException)
+    async def http_exception_handler(request: Request, exc: HTTPException):
+        log_tool.error(f"======================HTTP异常========================")
+        path = request.url.path
+        headers_dict = dict(request.headers)
+        headers = json.dumps(headers_dict)
+        query_params_dict = dict(request.query_params)
+        query_params = json.dumps(query_params_dict)
+        try:
+            body = getattr(request.state, 'body', None)
+        except Exception:
+            body = None
+
+        error_message = ''
+        if server_name:
+            error_message += f"服务名称: {server_name}\n"
+        error_message += f"请求路径: {path}\n请求头: {headers}\n请求查询参数: {query_params}\n请求体: {body}"
+        log_tool.error(error_message)
+        log_tool.error(f"{exc}", exc_info=True)
+
+        if lark_webhook_url:
+            error_message_with_stack = error_message + f"错误信息: {str(exc.detail)}\n 状态码: {exc.status_code}"
+            send_alert_to_feishu(lark_webhook_url, error_message_with_stack)
+
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"message": exc.detail},
         )
